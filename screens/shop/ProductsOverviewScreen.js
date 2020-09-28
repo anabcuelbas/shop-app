fda6d056f662e199b709a433f45ed9c3292835e5
anabcuelbas <abcuelbas@gmail.com>
2020-09-28 12:11:45 -0300

    Initialized Redux

---
+++ screens/shop/ProductsOverviewScreen.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Flatlist, StyleSheet } from 'reatc-native'
+
+const ProductsOverviewScreen = props => {
+    return (
+        <Flatlist />
+    )
+}
+
+const styles = StyleSheet.create({
+
+})
+
+export default ProductsOverviewScreen
